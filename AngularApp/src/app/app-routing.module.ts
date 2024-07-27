4453900d0cad7806ac392c1aa6f3d547ca1097e6
DanielIozia <danieliozia.di@gmail.com>
2024-07-27 17:11:10 +0200

    canActivate

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
 //guard
 import { authGuard } from './services/auth/auth.guard';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { 
-    path: 'home', component: HomeComponent,
+    path: 'home', component: HomeComponent, canActivate:[authGuard],
     loadChildren: () => import('./module/home/home.module').then(m => m.HomeModule),
   },
   { path: '404', component: PageNotFoundComponent },
